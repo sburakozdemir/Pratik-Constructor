f6f955ee403282a9c11097066711814192f72e64
sburakozdemir <sburakozdemir00@gmail.com>
2024-08-06 14:50:43 +0300

    Add project files.

---
+++ Pratik-Constructor/Baby.cs
@@ -0,0 +1,37 @@
+﻿// Baby sınıfı tanımı
+public class Baby
+{
+    // Özellikler (Properties)
+    public DateTime BirthDate { get; private set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+
+    // Parametresiz constructor
+    public Baby()
+    {
+        Initialize();
+    }
+
+    // Ad ve Soyad parametreli constructor
+    public Baby(string name, string lastName)
+    {
+        Name = name;
+        LastName = lastName;
+        Initialize();
+    }
+
+    // Her iki constructor'ın ortak işlemleri için yardımcı metod
+    private void Initialize()
+    {
+        Console.WriteLine("Ingaaaa");
+        BirthDate = DateTime.Now;
+    }
+
+    // Bebeğin bilgilerini yazdıran metod
+    public void Introduce()
+    {
+        Console.WriteLine($"Doğum Tarihi: {BirthDate}");
+        Console.WriteLine($"Adı: {Name}");
+        Console.WriteLine($"Soyadı: {LastName}");
+    }
+}
---
+++ Pratik-Constructor/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// Parametresiz constructor ile bebek oluşturma
+Baby baby1 = new Baby();
+baby1.Name = "Can";
+baby1.LastName = "Biricik";
+Console.WriteLine("Baby 1:");
+baby1.Introduce();
+
+Console.WriteLine();
+
+// Parametreli constructor ile bebek oluşturma
+Baby baby2 = new Baby("Melih", "Uçar");
+Console.WriteLine("Baby 2:");
+baby2.Introduce();
+    
